b2afc0336ec09e53374e5422b4f06294c3fa41ca
SrNexar <nfsalguero@uce.edu.ec>
2024-05-26 15:53:00 -0500

    Cambio Puerto

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// server.js
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+// Obtener el puerto de la variable de entorno PORT o usar el puerto 4000 por defecto
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-//hola
